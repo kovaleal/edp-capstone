a090a722420900619bbbdcf5c61829612cfcd18f
kovaleal <a.kovalenko1996@gmail.com>
2025-07-09 15:52:03 -0400

    catalog endpoint

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const mongo_url = process.env.MONGO_URI;
 
 const app = express();
 
+const Catalog = require('./catalog');
 const Product = require('./products');
 //TBD transactions
 //const Transaction = require('./transactions');
@@ -28,6 +29,36 @@ mongoose.connect(mongo_url)
 });
 
 // Routes
+// Fetch entire catalog (lightweight)
+app.get('/api/products/catalog', async (req, res) => {
+    try {
+        const catalog = await Catalog.find();
+        if (!catalog) {
+            return res.status(404).json({ error: 'Catalog unavailable' });
+        }
+        res.json(catalog);
+      } catch (error) {
+        console.error('Error fetching product catalog:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+});
+
+// Fetch 'num' products from catalog
+app.get('/api/products/catalog/:num', async (req, res) => {
+    const num = req.params.num;
+    try {
+        const catalog = await Catalog.find().sort({'_id': 1}).limit(num);
+        if (!catalog) {
+            return res.status(404).json({ error: 'Catalog unavailable' });
+        }
+        res.json(catalog);
+      } catch (error) {
+        console.error('Error fetching product catalog:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+});
+
+// Fetch complete item listing
 app.get('/api/products/:id', async (req, res) => {
     const id = req.params.id;
     try {
